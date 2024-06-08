78d3dd1785cc1a49d085e9746e0f02d841b78ccc
Dragiinus <melvinmaute@gmail.com>
2024-06-08 23:44:04 +0200

    Ajout img & config input

---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,9 @@
+export class Monster {
+  name: string = 'Monster';
+  hp: number = 60;
+  figureCaption: string = 'N°001 Monster';
+
+  attackName: string = 'Standard Attack';
+  attackStrength: number = 10;
+  attackDescription: string = 'This is a long description....';
+}
